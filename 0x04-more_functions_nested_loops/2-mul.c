8e8a55573e0c7427abee1b7caf8ddf36206082c0
DemDeeep <Demiladepeter321@gmail.com>
2023-12-22 14:24:32 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - mulitiples two int
+ * @c: function parameter 1
+ * @b: function parameter 2
+ * Returns: returns void
+ */
+
+int mul(int a, int b)
+{
+
+return (a * b);
+
+}
