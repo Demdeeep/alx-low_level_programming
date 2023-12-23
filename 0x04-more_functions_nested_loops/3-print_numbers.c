2909672c2812ce3aa817e3d840a28c849c52c545
DemDeeep <Demiladepeter321@gmail.com>
2023-12-22 16:13:42 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,16 @@
 #include "main.h"
-
 /**
  * print_numbers - print 0-9
  * return: void
  */
-
 void print_numbers(void)
 {
-	char c;
+int c;
+for (c = 0; c < 10; c++)
+{
+	_putchar(c + '0');
+}
 
-	for (c = '0'; c <= '9'; c++)
-	{
-	_putchar(c);
-	}
 	_putchar('\n');
 }
 
