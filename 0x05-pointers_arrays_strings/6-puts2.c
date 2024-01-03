58bc8e53bbcbd1ab99e76db61ca2d3a5d502eefe
DemDeeep <Demiladepeter321@gmail.com>
2024-01-03 13:10:36 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * puts2 - function should print only one character out of two
- * starting with the first one 
- * @str: input 
- * Return: print 
+ * starting with the first one
+ * @str: input
+ * Return: print
  */
 void puts2(char *str)
 {
